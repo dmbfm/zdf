14af49dce2dca668541efad282ae3187532b22d3
Daniel Fortes <daniel.mbfm@gmail.com>
2021-11-16 20:01:49 -0300

    Added Args.has function

--- src/args.zig
+++ src/args.zig
@@ -7,7 +7,7 @@ const expect = testing.expect;
 /// builds upfront an slice containing all of the arguments so that they can be
 /// accessed directly.
 const Args = struct {
-    argv: [][:0]u8,
+    argv: [][]u8,
     argc: usize,
     allocator: *Allocator,
 
@@ -19,7 +19,7 @@ const Args = struct {
             n += 1;
         }
 
-        var args = try allocator.alloc([:0]u8, n);
+        var args = try allocator.alloc([]u8, n);
 
         it = std.process.args();
 
@@ -43,7 +43,30 @@ const Args = struct {
 
         self.allocator.free(self.argv);
     }
+
+    pub fn has(self: Args, arg: []const u8) bool {
+        for (self.argv) |current| {
+            if (std.mem.eql(u8, current, arg)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 };
+
+pub fn addArg(self: *Args, arg: []const u8) !void {
+    var argCopy = try self.allocator.alloc(u8, arg.len);
+    @memcpy(argCopy.ptr, arg.ptr, arg.len);
+
+    var newArgv = try self.allocator.alloc([]u8, self.argc + 1);
+    @memcpy(std.mem.sliceAsBytes(newArgv).ptr, std.mem.sliceAsBytes(self.argv).ptr, self.argc * @sizeOf([]u8));
+    self.allocator.free(self.argv);
+    self.argv = newArgv;
+
+    self.argv[self.argc] = argCopy;
+    self.argc += 1;
+}
 
 test "Args" {
     var args = try Args.init(std.testing.allocator);
@@ -51,7 +74,13 @@ test "Args" {
 
     try expect(args.argc == 2);
 
+    try addArg(&args, "test");
+    try expect(args.argc == 3);
+
     var end: usize = args.argv[1].len;
     var start = end - 3;
     try expect(std.mem.eql(u8, args.argv[1][start..end], "zig"));
+
+    try expect(std.mem.eql(u8, args.argv[2], "test"));
+    try expect(args.has("test"));
 }
